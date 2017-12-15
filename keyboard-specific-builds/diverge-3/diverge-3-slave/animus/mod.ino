31384c7d150c5d1cb9fbd0a1f3beebf2321e4d85
scauligi <scauligi@eng.ucsd.edu>
2017-12-15 08:11:18 -0800

    build for diverge 3

--- keyboard-specific-builds/diverge-3/diverge-3-slave/animus/mod.ino
+++ keyboard-specific-builds/diverge-3/diverge-3-slave/animus/mod.ino
@@ -1,8 +1,10 @@
-#define builder_mstartup BledPWMStartup(); I2CSlaveStartup(); dualRolesStartup(); mediaStartup(); macroStartup(); stickyKeysStartup(); tapDanceStartup();
-#define builder_mloop BledPWMLoop(); I2CSlaveLoop(); dualRolesLoop(); mediaLoop(); macroLoop(); stickyKeysLoop(); tapDanceLoop();
-#define builder_mkeydown BledPWMKeyDown(val, type); I2CSlaveKeyDown(val, type); dualRolesKeyDown(val, type); mediaKeyDown(val, type); macroKeyDown(val, type); stickyKeysKeyDown(val, type); tapDanceKeyDown(val, type);
-#define builder_mkeyup BledPWMKeyUp(val, type); I2CSlaveKeyUp(val, type); dualRolesKeyUp(val, type); mediaKeyUp(val, type); macroKeyUp(val, type); stickyKeysKeyUp(val, type); tapDanceKeyUp(val, type);
-#define builder_mserial BledPWMSerial(input); I2CSlaveSerial(input); dualRolesSerial(input); mediaSerial(input); macroSerial(input); stickyKeysSerial(input); tapDanceSerial(input);
+#define builder_mstartup BledPWMStartup(); I2CSlaveStartup(); dualRolesStartup(); mediaStartup(); macroStartup(); stickyKeysStartup(); tapDanceStartup(); mouseKeysStartup(); shiftedStartup();
+#define builder_mloop BledPWMLoop(); I2CSlaveLoop(); dualRolesLoop(); mediaLoop(); macroLoop(); stickyKeysLoop(); tapDanceLoop(); mouseKeysLoop(); shiftedLoop();
+#define builder_mpresscoord BledPWMPressCoord(x, y); I2CSlavePressCoord(x, y); dualRolesPressCoord(x, y); mediaPressCoord(x, y); macroPressCoord(x, y); stickyKeysPressCoord(x, y); tapDancePressCoord(x, y); mouseKeysPressCoord(x, y); shiftedPressCoord(x, y);
+#define builder_mprepress BledPWMPrePress(val, type); I2CSlavePrePress(val, type); dualRolesPrePress(val, type); mediaPrePress(val, type); macroPrePress(val, type); stickyKeysPrePress(val, type); tapDancePrePress(val, type); mouseKeysPrePress(val, type); shiftedPrePress(val, type);
+#define builder_mkeydown BledPWMKeyDown(val, type); I2CSlaveKeyDown(val, type); dualRolesKeyDown(val, type); mediaKeyDown(val, type); macroKeyDown(val, type); stickyKeysKeyDown(val, type); tapDanceKeyDown(val, type); mouseKeysKeyDown(val, type); shiftedKeyDown(val, type);
+#define builder_mkeyup BledPWMKeyUp(val, type); I2CSlaveKeyUp(val, type); dualRolesKeyUp(val, type); mediaKeyUp(val, type); macroKeyUp(val, type); stickyKeysKeyUp(val, type); tapDanceKeyUp(val, type); mouseKeysKeyUp(val, type); shiftedKeyUp(val, type);
+#define builder_mserial BledPWMSerial(input); I2CSlaveSerial(input); dualRolesSerial(input); mediaSerial(input); macroSerial(input); stickyKeysSerial(input); tapDanceSerial(input); mouseKeysSerial(input); shiftedSerial(input);
 
 void ModStartup()
 {
@@ -17,6 +19,16 @@ void ModLoop()
   builder_mloop
 }
 
+
+void ModPressCoord(byte x, byte y)
+{
+  builder_mpresscoord
+}
+
+void ModPrePress(char val, byte type)
+{
+  builder_mprepress
+}
 
 void ModKeyDown(char val, byte type)
 {
