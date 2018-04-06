37a7c2123a71684cc85441476caf20de178ee116
blahlicus <blahlicus@gmail.com>
2018-04-07 03:30:35 +0800

    add: macros

--- animus-3/source/animus/Mod.h
+++ animus-3/source/animus/Mod.h
@@ -1,13 +1,13 @@
 #ifndef ANIMUS_MOD
 #define ANIMUS_MOD
 
-#define builder_mod_begin ModDualRoles.Begin(); ModModifier.Begin();  ModSticky.Begin(); ModMedia.Begin();
-#define builder_mod_loop ModDualRoles.Loop(); ModModifier.Loop();  ModSticky.Loop(); ModMedia.Loop();
-#define builder_mod_loaddata ModDualRoles.LoadData(); ModModifier.LoadData();  ModSticky.LoadData(); ModMedia.LoadData();
-#define builder_mod_prepress ModDualRoles.PrePress(val, type); ModModifier.PrePress(val, type);  ModSticky.PrePress(val, type); ModMedia.PrePress(val, type);
-#define builder_mod_press ModDualRoles.PressKey(val, type); ModModifier.PressKey(val, type);  ModSticky.PressKey(val, type); ModMedia.PressKey(val, type);
-#define builder_mod_release ModDualRoles.ReleaseKey(val, type); ModModifier.ReleaseKey(val, type);  ModSticky.ReleaseKey(val, type); ModMedia.ReleaseKey(val, type);
-#define builder_mod_print ModDualRoles.PrintMods(); ModModifier.PrintMods();  ModSticky.PrintMods(); ModMedia.PrintMods();
+#define builder_mod_begin ModDualRoles.Begin(); ModModifier.Begin();  ModSticky.Begin(); ModMedia.Begin(); ModMacro.Begin();
+#define builder_mod_loop ModDualRoles.Loop(); ModModifier.Loop();  ModSticky.Loop(); ModMedia.Loop(); ModMacro.Loop();
+#define builder_mod_loaddata ModDualRoles.LoadData(); ModModifier.LoadData();  ModSticky.LoadData(); ModMedia.LoadData(); ModMacro.LoadData();
+#define builder_mod_prepress ModDualRoles.PrePress(val, type); ModModifier.PrePress(val, type);  ModSticky.PrePress(val, type); ModMedia.PrePress(val, type); ModMacro.PrePress(val, type);
+#define builder_mod_press ModDualRoles.PressKey(val, type); ModModifier.PressKey(val, type);  ModSticky.PressKey(val, type); ModMedia.PressKey(val, type); ModMacro.PressKey(val, type);
+#define builder_mod_release ModDualRoles.ReleaseKey(val, type); ModModifier.ReleaseKey(val, type);  ModSticky.ReleaseKey(val, type); ModMedia.ReleaseKey(val, type); ModMacro.ReleaseKey(val, type);
+#define builder_mod_print ModDualRoles.PrintMods(); ModModifier.PrintMods();  ModSticky.PrintMods(); ModMedia.PrintMods(); ModMacro.PrintMods();
 
 
 // mod dependencies below
@@ -15,6 +15,7 @@
 #include "ModDualRoles.h"
 #include "ModSticky.h"
 #include "ModMedia.h"
+#include "ModMacro.h"
 // mod dependencies above
 
 
---
+++ animus-3/source/animus/ModMacro.cpp
@@ -0,0 +1,97 @@
+#include "ModMacro.h"
+
+CModMacro::CModMacro(void)
+{
+  // nothing
+}
+
+void CModMacro::Begin(void)
+{
+  CModTemplate::Begin();
+  ModGUID = 5; // GUID of this specific mod
+}
+
+void CModMacro::LoadData(void)
+{
+  CModTemplate::LoadData();
+
+  MacroCount = GetData(0);
+  KeyDelay = GetData(1);
+}
+
+void CModMacro::PressMacro(byte id)
+{
+  short startingAddr = 2;
+  for (byte i = 0; i < id; i++) // this retrieves the starting address of the specific macro
+  {
+    startingAddr = startingAddr + 1 + GetData(startingAddr) * 2;
+  }
+
+  byte macroLength = GetData(startingAddr);
+
+  for (byte i = 0; i < macroLength; i++) // iterates and executes the macros
+  {
+    Animus.PressKey(GetData(startingAddr + 1 + i * 2), GetData(startingAddr + 2 + i * 2));
+    delay(KeyDelay);
+  }
+}
+
+void CModMacro::ReleaseMacro(byte id)
+{
+  short startingAddr = 2;
+  for (byte i = 0; i < id; i++) // this retrieves the starting address of the specific macro
+  {
+    startingAddr = startingAddr + 1 + GetData(startingAddr) * 2;
+  }
+
+  byte macroLength = GetData(startingAddr);
+
+  for (byte i = 0; i < macroLength; i++) // iterates and releases the keys in the macro backwards
+  {
+    Animus.ReleaseKey(GetData(startingAddr + 1 + (macroLength - 1 - i) * 2), GetData(startingAddr + 2 + (macroLength - 1 - i) * 2));
+    delay(KeyDelay);
+  }
+}
+
+void CModMacro::Loop(void)
+{
+  CModTemplate::Loop();
+}
+
+void CModMacro::PrePress(byte val, byte type)
+{
+  CModTemplate::PrePress(val, type);
+}
+void CModMacro::PressKey(byte val, byte type)
+{
+  CModTemplate::PressKey(val, type);
+
+  if (Global.HasUSB)
+  {
+    if (type == 32)
+    {
+      PressMacro(val);
+    }
+  }
+}
+void CModMacro::ReleaseKey(byte val, byte type)
+{
+  CModTemplate::ReleaseKey(val, type);
+
+  if (Global.HasUSB)
+  {
+
+    if (type == 32)
+    {
+      ReleaseMacro(val);
+    }
+  }
+}
+
+void CModMacro::SerialComms(byte mode)
+{
+  CModTemplate::SerialComms(mode);
+}
+
+
+CModMacro ModMacro;
