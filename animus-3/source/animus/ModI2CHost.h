2b7d544f222639fa4f9e1c2f87f1afafab3a6251
blahlicus <blahlicus@gmail.com>
2018-04-21 19:11:23 +0800

    add: i2chost progress

--- animus-3/source/animus/ModI2CHost.h
+++ animus-3/source/animus/ModI2CHost.h
@@ -2,6 +2,25 @@
 #define ANIMUS_MOD_ModI2CHost
 #include "ModTemplate.h"
 #include "Animus.h"
+#include "Wire.h"
+
+
+
+
+
+/* references
+below are types of i2c transmissions from the host to guest
+1: set TempLayer (type, tlayer)
+2: set KeyLayer (type, klayer)
+3: set eeprom data (packages of more than 30 bytes) (type, byte0, byte1, ...)
+4: EOL for setting eeprom data, if setting less than 30 bytes, use this (type, length, byte0, byte 1, ...)
+5: change max layer count (type, maxlay)
+6: change refresh rate (type, refreshRate)
+7: change board type (type, boardType)
+8: change rows and row count (type, rowCount, rowPin0, rowPin1, ...)
+9: change cols and col count (type, colCount, colPin0, colPin1, ...)
+10: change brightness (type, brightness value)
+*/
 
 class CModI2CHost : public CModTemplate
 {
